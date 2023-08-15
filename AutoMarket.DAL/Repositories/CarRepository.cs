7697a78d929942639767285c235b5fa6cc47f91f
SuhrobJumaev <jumaevsuhrob102@gmail.com>
2023-08-15 15:13:08 +0500

    Added Repositories

---
+++ AutoMarket.DAL/Repositories/CarRepository.cs
@@ -0,0 +1,46 @@
+﻿using AutoMarket.DAL.Interfaces;
+using AutoMarket.Domain.Entity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoMarket.DAL.Repositories
+{
+    public class CarRepository : ICarRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CarRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public bool Create(Car entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Car Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<Car>> GetAll()
+        {
+            return await _db.car.ToListAsync();
+        }
+
+        public Car GetByName(string name)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
